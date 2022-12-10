9a1c990055aa0495d4c5236f4ce4f513278430c4
shayv <vaturis@gmail.com>
2022-12-10 18:03:52 +0200

    code formatting.

--- DesignPatterns.Builder.Problem/Program.cs
+++ DesignPatterns.Builder.Problem/Program.cs
@@ -1,7 +1,25 @@
 ﻿using DesignPatterns.Builder.Model;
 
-Car familyCar = new(new EngineFourCylinders() { Capacity = 1500 }, new Seats { Count = 5 }, 6);
-Car sportCar = new(new EngineV8() { Capacity = 3000 }, new Seats { Count = 4 }, 7, true, true, true);
+Car familyCar = new(
+    new EngineFourCylinders()
+    {
+        Capacity = 1500
+    },
+    new Seats
+    {
+        Count = 5
+    },
+    6);
+
+Car sportCar = new(
+    new EngineV8()
+    {
+        Capacity = 3000
+    },
+    new Seats
+    {
+        Count = 4
+    }, 7, true, true, true);
 
 Console.WriteLine($"Family Car: {familyCar}");
 Console.WriteLine($"Sport Car: {sportCar}");
